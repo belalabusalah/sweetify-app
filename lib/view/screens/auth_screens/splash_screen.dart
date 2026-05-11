3e67dc3afd919c7225a923cb9afa4ed46c963edd
belalabusalah <belalabusonia@gmail.com>
2026-05-11 18:53:48 +0300

    Initialize project packages and setup

---
+++ lib/view/screens/auth_screens/splash_screen.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width:.infinity,
+        height: .infinity,
+        decoration: BoxDecoration(
+          color: Colors.red,
+        ),
+        child: Column(
+          children: [
+
+        ],),
+      ),
+    );
+  }
+}
