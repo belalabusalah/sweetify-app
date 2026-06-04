d81c0e8d4a8066ee6539f0be2777eaea296c392e
belalabusalah <belalabusonia@gmail.com>
2026-06-04 12:48:04 +0300

    create details screen and link with api

--- lib/view/screens/detils_screen.dart
+++ lib/view/screens/detils_screen.dart
@@ -1,25 +1,263 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:sweetify_app/view/widgets/elevated_button_app_custom.dart';
 
-import '../widgets/menu_section.dart';
+import '../../../model/get_product/product_model.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+  DetailsScreen({super.key});
+
+  final ProductModel product = Get.arguments as ProductModel;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("DetilsScreen")),
-      drawer: CustomDrawer(),
-      body: Center(
-        child: Container(
-          child: ElevatedButton(
-            onPressed: () {
-              Get.toNamed("/profileScreen");
-            },
-            child: Text("login".tr),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(16.w),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(blurRadius: 10, color: Colors.black12)],
+        ),
+        child: Row(
+          spacing: 4.w,
+          children: [
+            Icon(
+              Icons.shopping_cart_outlined,
+              size: 24,
+              color: Color(0xFF483028),
+            ),
+            Container(
+              width: 110.w,
+              height: 40.h,
+              decoration: BoxDecoration(
+                color: Color(0xFFF4F4F4),
+                borderRadius: BorderRadius.circular(50.r),
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Text(
+                      "-",
+                      style: TextStyle(
+                        color: Color(0xFF1C1C1E),
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14.sp,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "1",
+                    style: TextStyle(
+                      color: Color(0xFF1C1C1E),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16.sp,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF1C1C1E),
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Expanded(
+              child: CustomElevatedButton(
+                text: 'Add To Cart',
+                fontSizeText: 16.sp,
+                height: 45.h,
+                width: 300.w,
+                icon: Icons.card_travel,
+                color: Colors.orange,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      /* Container(
+        padding: EdgeInsets.all(16.w),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10,
+              color: Colors.black12,
+            ),
+          ],
+        ),
+        child: SizedBox(
+          height: 55.h,
+          child: ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart_outlined),
+            label: const Text("Add To Cart"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orange,
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
           ),
         ),
+      ),*/
+      /*Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Icon(
+              Icons.shopping_cart_outlined,
+              size: 20,
+              color: Color(0xFF483028),
+            ),
+            CustomElevatedButton(
+              text: 'Add To Cart',
+              fontSizeText: 16.sp,
+              height: 55.h,
+              icon: Icons.card_travel,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),*/
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 350.h,
+            pinned: true,
+            backgroundColor: Colors.white,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () => Get.back(),
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.favorite_border, color: Colors.red),
+                onPressed: () {
+                  // Handle favorite button press .
+                },
+              ),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              background: Hero(
+                tag: product.id,
+                child: Image.network(product.thumbnail, fit: BoxFit.cover),
+              ),
+            ),
+          ),
+
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(20.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: TextStyle(
+                      fontSize: 22.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 8.h),
+
+                  Row(
+                    children: [
+                      Text(
+                        "${product.finalUnitPrice} KWD",
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      const Spacer(),
+
+                      Icon(Icons.star, color: Colors.amber, size: 18.sp),
+
+                      SizedBox(width: 4.w),
+
+                      Text("4.8"),
+                    ],
+                  ),
+
+                  SizedBox(height: 25.h),
+
+                  Text(
+                    "Description",
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  SizedBox(height: 10.h),
+
+                  Text(
+                    product.shortDescription,
+                    style: TextStyle(height: 1.6, color: Colors.grey[700]),
+                  ),
+
+                  SizedBox(height: 25.h),
+
+                  Text(
+                    "Product Details",
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  SizedBox(height: 10.h),
+
+                  Container(
+                    padding: EdgeInsets.all(16.w),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      children: [
+                        _item("Product ID", product.id.toString()),
+                        _item("Price", "${product.finalUnitPrice} KWD"),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 80.h),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _item(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          Text(title),
+          const Spacer(),
+          Text(value, style: const TextStyle(fontWeight: FontWeight.bold)),
+        ],
       ),
     );
   }
--- lib/view/screens/home_screen/home_product_section.dart
+++ lib/view/screens/home_screen/home_product_section.dart
@@ -27,109 +27,114 @@ class HomeProductSection extends StatelessWidget {
           physics: NeverScrollableScrollPhysics(),
           itemBuilder: (context, index) {
             final product = _homeController.products[index];
-            return Container(
-              margin: EdgeInsets.symmetric(vertical: 4, horizontal: 4),
+            return InkWell(
+              onTap: () {
+                Get.toNamed("/detailsScreen", arguments: product);
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 4, horizontal: 4),
 
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(5, 5),
-                    color: Colors.black12,
-                    blurRadius: 3,
-                  ),
-                  BoxShadow(
-                    offset: Offset(-5, -5),
-                    color: Colors.black12,
-                    blurRadius: 3,
-                  ),
-                  BoxShadow(
-                    offset: Offset(5, -5),
-                    color: Colors.black12,
-                    blurRadius: 3,
-                  ),
-                  BoxShadow(
-                    offset: Offset(-5, 5),
-                    color: Colors.white12,
-                    blurRadius: 3,
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Stack(
-                    children: [
-                      Image.network(
-                        product.thumbnail, // 🔥 من API
-                        height: 100.h,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                      Positioned(
-                        top: 10.h,
-                        right: 10.w,
-                        child: Container(
-                          height: 30.h,
-                          width: 37.w,
-                          clipBehavior: Clip.antiAlias,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: IconButton(
-                            icon: Icon(Icons.favorite, color: Colors.red),
-                            onPressed: () {},
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 10.h,
-                        left: 10.w,
-                        child: Container(
-                          height: 30.h,
-                          width: 37.w,
-                          clipBehavior: Clip.antiAlias,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.shopping_cart_outlined,
-                              color: Colors.red,
-                            ),
-                            onPressed: () {},
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 18),
-                    child: Column(
-                      spacing: 8,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(5, 5),
+                      color: Colors.black12,
+                      blurRadius: 3,
+                    ),
+                    BoxShadow(
+                      offset: Offset(-5, -5),
+                      color: Colors.black12,
+                      blurRadius: 3,
+                    ),
+                    BoxShadow(
+                      offset: Offset(5, -5),
+                      color: Colors.black12,
+                      blurRadius: 3,
+                    ),
+                    BoxShadow(
+                      offset: Offset(-5, 5),
+                      color: Colors.white12,
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Stack(
                       children: [
-                        AppText.caption(product.name, maxLines: 3),
-                        AppText.body(
-                          product.shortDescription,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                          fontSize: 10,
+                        Image.network(
+                          product.thumbnail, // 🔥 من API
+                          height: 100.h,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
                         ),
-                        AppText.subtitle(
-                          "${product.finalUnitPrice} KWD",
-                          fontSize: 12,
+                        Positioned(
+                          top: 10.h,
+                          right: 10.w,
+                          child: Container(
+                            height: 30.h,
+                            width: 37.w,
+                            clipBehavior: Clip.antiAlias,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: IconButton(
+                              icon: Icon(Icons.favorite, color: Colors.red),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 10.h,
+                          left: 10.w,
+                          child: Container(
+                            height: 30.h,
+                            width: 37.w,
+                            clipBehavior: Clip.antiAlias,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.shopping_cart_outlined,
+                                color: Colors.red,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 18),
+                      child: Column(
+                        spacing: 8,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          AppText.caption(product.name, maxLines: 3),
+                          AppText.body(
+                            product.shortDescription,
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 10,
+                          ),
+                          AppText.subtitle(
+                            "${product.finalUnitPrice} KWD",
+                            fontSize: 12,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           },
