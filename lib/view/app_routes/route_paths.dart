344d3fb7a9a1a4c213c4ae591c82da2861a6454b
belalabusalah <belalabusonia@gmail.com>
2026-07-13 12:09:14 +0300

    refactor: clean up screen routes

--- lib/view/app_routes/route_paths.dart
+++ lib/view/app_routes/route_paths.dart
@@ -5,5 +5,20 @@ class RoutePaths {
   static const String determineEntryScreen = "/determineEntryScreen";
   static const String signInScreen = "/signInScreen";
   static const String signUpScreen = "/signUpScreen";
+  static const String homeScreen = "/homeScreen";
+  static const String profileScreen = "/profileScreen";
+  static const String aboutUsScreen = "/aboutUsScreen";
+  static const String privacyPolicyScreen = "/privacyPolicyScreen";
+  static const String termsConditionsScreen = "/termsConditionsScreen";
+  static const String supportScreen = "/supportScreen";
+  static const String addressScreen = "/addressScreen";
+  static const String addressFormScreen = "/addressFormScreen";
+  static const String wishListScreen = "/wishListScreen";
+  static const String paymentScreen = "/paymentScreen";
+  static const String detailsScreen = "/detailsScreen";
+  static const String cardScreen = "/cardScreen";
+  static const String mainScreen = "/mainScreen";
+  static const String shoppingScreen = "/shoppingScreen";
+  static const String checkoutScreen = "/checkoutScreen";
 
 }
